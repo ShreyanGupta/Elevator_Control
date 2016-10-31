55e87c606a26a9885c82c238afc72e3915223e1a
Vaibhav Bhagee <vaibhavbhagee@gmail.com>
2016-10-31 22:44:41 +0530

    simulator tested. seems to work fine now.

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -1,5 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <random>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string>
+#include <assert.h>
+
+using namespace std;
+
 float random_fraction();
-float random_int();
+float random_int(int n);
 
 class person
 {
@@ -44,9 +58,11 @@ class person
 
 			if (exit_flr > entry)
 				up = true;
+
+			// cout<<"********************************** "<<n<<" EXIT: "<<exit_flr<<" ENTRY: "<<entry<<endl;
 		}
 
-		~person();
+		// ~person();
 		
 		void update_elevator(int i);
 };
@@ -78,7 +94,7 @@ class lift
 			}
 		}
 
-		~lift();
+		// ~lift();
 		
 		void change_direction(bool up)
 		{
@@ -103,7 +119,7 @@ class lift
 
 		int get_person_count()
 		{
-			return persons.size();
+			return people.size();
 		}
 };
 
@@ -154,7 +170,7 @@ class simulator
 			for(int i = 0; i<k; i++)
 			{
 				lift el(i+1,n);
-				elevator.push_back(el);
+				elevators.push_back(el);
 			}
 
 			for(int i = 0; i<n; i++)
@@ -170,7 +186,9 @@ class simulator
 			return this -> cost;
 		}
 
-		~simulator();
+		void get_state_details();
+
+		// ~simulator();
 	
 };
 
@@ -277,38 +295,38 @@ string simulator::parse_and_take_action(string act)
 			if (actions[i].compare(0,2,"AU") == 0) // move up
 			{
 				int liftid = stoi(actions[i].substr(2)) - 1;
-				elevator[liftid].floor++;
+				elevators[liftid].floor++;
 				curr_cost +=1;
 			}
 			else if (actions[i].compare(0,2,"AD") == 0) // move up
 			{
 				int liftid = stoi(actions[i].substr(2)) - 1;
-				elevator[liftid].floor--;
+				elevators[liftid].floor--;
 				curr_cost +=1;
 			}
 			if (actions[i].compare(0,3,"AOU") == 0) // move up
 			{
 				int liftid = stoi(actions[i].substr(3)) - 1;
-				elevator[liftid].remove_persons_with_dest();
-				elevator[liftid].unpress_button(elevator[liftid].floor);
+				elevators[liftid].remove_persons_with_dest();
+				elevators[liftid].unpress_button(elevators[liftid].floor);
 
-				int temp = building[elevator[liftid].floor].size();
-				press_up[elevator[liftid].floor] = false;
+				int temp = building[elevators[liftid].floor].size();
+				press_up[elevators[liftid].floor] = false;
 
 				for(int j = 0; j<temp; j++)
 				{
-					person per = building[elevator[liftid].floor].front();
-					building[elevator[liftid].floor].pop();
+					person per = building[elevators[liftid].floor].front();
+					building[elevators[liftid].floor].pop();
 
-					if (per.entry == elevator[liftid].floor && per.up)
+					if (per.entry == elevators[liftid].floor && per.up)
 					{
 						per.elevator = liftid;
-						elevator[liftid].add_person(per);
-						if (!elevator[liftid].buttons[per.exit_flr])
+						elevators[liftid].add_person(per);
+						if (!elevators[liftid].buttons[per.exit_flr])
 						{
-							elevator[liftid].buttons[per.exit_flr] = true;
+							elevators[liftid].buttons[per.exit_flr] = true;
 							observations += "B_";
-							observations +=	to_string(elevator[liftid].floor+1); // starting from 1
+							observations +=	to_string(per.exit_flr+1); // starting from 1
 							observations += "_";
 							observations += to_string(liftid+1);
 							observations += " ";
@@ -316,33 +334,33 @@ string simulator::parse_and_take_action(string act)
 					}
 					else
 					{
-						building[elevator[liftid].floor].push(per);
+						building[elevators[liftid].floor].push(per);
 					}
 				}
 			}
 			else if (actions[i].compare(0,3,"AOD") == 0) // move up
 			{
 				int liftid = stoi(actions[i].substr(3)) - 1;
-				elevator[liftid].remove_persons_with_dest();
-				elevator[liftid].unpress_button(elevator[liftid].floor);
+				elevators[liftid].remove_persons_with_dest();
+				elevators[liftid].unpress_button(elevators[liftid].floor);
 
-				int temp = building[elevator[liftid].floor].size();
-				press_down[elevator[liftid].floor] = false;
+				int temp = building[elevators[liftid].floor].size();
+				press_down[elevators[liftid].floor] = false;
 
 				for(int j = 0; j<temp; j++)
 				{
-					person per = building[elevator[liftid].floor].front();
-					building[elevator[liftid].floor].pop();
+					person per = building[elevators[liftid].floor].front();
+					building[elevators[liftid].floor].pop();
 
-					if (per.entry == elevator[liftid].floor && !per.up)
+					if (per.entry == elevators[liftid].floor && !per.up)
 					{
 						per.elevator = liftid;
-						elevator[liftid].add_person(per);
-						if (!elevator[liftid].buttons[per.exit_flr])
+						elevators[liftid].add_person(per);
+						if (!elevators[liftid].buttons[per.exit_flr])
 						{
-							elevator[liftid].buttons[per.exit_flr] = true;
+							elevators[liftid].buttons[per.exit_flr] = true;
 							observations += "B_";
-							observations +=	to_string(elevator[liftid].floor+1); // starting from 1
+							observations +=	to_string(per.exit_flr+1); // starting from 1
 							observations += "_";
 							observations += to_string(liftid+1);
 							observations += " ";
@@ -350,7 +368,7 @@ string simulator::parse_and_take_action(string act)
 					}
 					else
 					{
-						building[elevator[liftid].floor].push(per);
+						building[elevators[liftid].floor].push(per);
 					}
 				}
 			}
@@ -411,6 +429,24 @@ string simulator::parse_and_take_action(string act)
 	}
 }
 
+void simulator::get_state_details()
+{
+	cout<<"COST: "<<curr_cost<<endl;
+	cout<<"PERSON INFORMATION:"<<endl;
+
+	for(int i = 0; i<n; i++)
+	{
+		cout<<"FLOOR "<<i+1<<": "<<building[i].size()<<endl;
+	}
+
+	cout<<"LIFT INFORMATION: "<<endl;
+
+	for(int i = 0; i<k; i++)
+	{
+		cout<<"LIFT "<<i+1<<": "<<elevators[i].get_person_count()<<" people at "<<elevators[i].floor<<endl;
+	}
+}
+
 float random_fraction()
 {
 	static std::default_random_engine generator (std::time(NULL));	
@@ -427,8 +463,35 @@ float random_int(int a)
 	static std::default_random_engine generator (std::time(NULL));	
   	static std::uniform_int_distribution<int> distribution(1,a);
 
-  	int num = distribution(generator);
+  	int num = (distribution(generator) % a) + 1;
 
+  	assert(num<=a);
   	// return num/INT_MAX;
   	return num/*/(float)INT_MAX*/;
 }
+
+// int main(int argc, char const *argv[])
+// {
+// 	// cout<<"yo"<<endl;
+
+// 	simulator sim(5,2,0.7,0.25,0.25);
+
+// 	// string s = "";
+// 	char inp[100];
+// 	string s = "";
+// 	while(cin.getline(inp,100))
+// 	{
+// 		// cin>>s;
+// 		// cout<<s<<endl;
+// 		for(int i = 0; inp[i] != '\0'; i++)
+// 		{
+// 			s+=inp[i];
+// 		}
+
+// 		cout<<sim.parse_and_take_action(s)<<endl;
+// 		sim.get_state_details();
+// 		s.clear();
+// 	}
+
+// 	return 0;
+// }
